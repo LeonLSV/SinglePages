20fdb78166679581bfb02049608f6dbd42686c09
Ramiro Contreras León <racramiro@gmail.com>
2020-08-24 20:20:11 -0500

    Ahora si funciona todo

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,19 @@
-import Header from '../templates/Header';
-import Home from '../pages/Home';
-import Character from '../pages/Character';
-import Error404 from '../pages/Error404';
-import getHash from '../utils/getHash';
-import resolveRoutes from '../utils/resolveRoutes';
+import Header from "../templates/Header";
+import Home from "../pages/Home";
+import Character from "../pages/Character";
+import Error404 from "../pages/Error404";
+import getHash from "../utils/getHash";
+import resolveRoutes from "../utils/resolveRoutes";
 
 const routes = {
-  '/': Home,
-  '/:id': Character,
-  '/contact': 'Contact',
+  "/": Home,
+  "/:id": Character,
+  "/contact": "Contact",
 };
 
 const router = async () => {
-  const header = null || document.getElementById('header');
-  const content = null || document.getElementById('content');
+  const header = null || document.getElementById("header");
+  const content = null || document.getElementById("content");
 
   header.innerHTML = await Header();
   let hash = getHash();
